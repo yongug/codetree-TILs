0d09e5c69df6877a8a7bcd49bec8c884399b16ad
Crong <102286049+yongug@users.noreply.github.com>
2024-07-29 12:23:09 +0900

    Title: 가장 짧은 부분합, Time: 5057ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240729/가장 짧은 부분합/shortest-subtotal.py
+++ 240729/가장 짧은 부분합/shortest-subtotal.py
@@ -1,18 +1,20 @@
 n, s = list(map(int, input().split() ))
-
+#print(n,s)
 num = list(map(int,input().split()))
+num.insert(0,0)
+#print(num)
+ans = 1000000
 
-ans = 100001
-
-for i in range(0,n):
+for i in range(1,n+1):
     sum_val = 0
-    for j in range(i+1,n):
+    for j in range(i,n+1):
         sum_val += num[j]
 
         if sum_val >= s:
             break
         
-    ans = min(ans,j-i+2)
+    if sum_val >=s:
+        ans = min(ans,j-i+1)
 
 if ans == 100001:
     print(-1)
